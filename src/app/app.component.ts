70dfb9b3d88b6de83253492d41330ab5a5fdefa1
Yati Gautam <yati.gautam@walkingtree.tech>
2025-05-09 15:57:15 +0530

    sibling components sharing data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { InfoBoxComponent } from './info-box/info-box.component';
-import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, InfoBoxComponent, RouterModule, LoginComponent],
+  imports: [InfoBoxComponent, LoginComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   sharedData: string = 'Shared data from AppComponent';
+
   updateSharedData(data: string) {
-    this.sharedData = data;
-  }
-  title = 'Angular-training';
-  onInfoBoxClose(data: string) {
-    console.log('Data received from InfoBoxComponent:', data);
-    alert('User Input:${data}');
+    this.sharedData =data ;
+    console.log('Data received in app component', data);
+  this.sharedData = data;
   }
 }

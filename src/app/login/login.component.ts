1768087361aba0994735cc38833881e314524bd2
Yati Gautam <yati.gautam@walkingtree.tech>
2025-05-12 14:20:37 +0530

    practice of angular directives

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { InfoBoxComponent } from '../info-box/info-box.component';
@@ -11,11 +11,20 @@ import { InfoBoxComponent } from '../info-box/info-box.component';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
   userName: string = '';
   name: string = '';
   email: string = '';
   password: string = '';
 
+isHighlighted: boolean =true;
+
+  features:string[]=['Two-way data binding', 'Property binding', 'Event binding', 'ngModel', 'ngIf', 'ngFor'];
+  
+  @Input() transformedData: string='';
+  @Input() requiredData!: string;
+  @Input() aliasName!: string;
+  @Input() sharedData: string = 'Shared data from AppComponent';
   onSubmit() {
     if (this.name && this.email && this.password) {
       alert('Form submitted successfully!');
